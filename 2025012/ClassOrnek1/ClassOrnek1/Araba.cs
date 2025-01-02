dfeace0e8f9f93b7bd02d11f34543973cea34c8a
Zt1918 <turkzt568@gmail.com>
2025-01-02 16:05:58 +0300

    Add files via upload

---
+++ 2025012/ClassOrnek1/ClassOrnek1/Araba.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassOrnek1
+{
+    internal class Araba
+    {
+        string marka;
+        string model;
+        string yakıtturu;
+        decimal fiyat;
+
+        public string Marka { get => marka; set => marka = value; }
+        public string Model { get => model; set => model = value; }
+        public string Yakıtturu { get => yakıtturu; set => yakıtturu = value; }
+        public decimal Fiyat { get => fiyat; set => fiyat = value; }
+
+        public Araba(string marka, string model, string yakıtturu, decimal fiyat)
+        {
+            Marka = marka;
+            Model = model;
+            Yakıtturu = yakıtturu;
+            Fiyat = fiyat;
+        }
+        public string BilgilerYazdir()
+        {
+            string s= $"Marka: {Marka}\nModel{Model}\nYakıt Türü:{Yakıtturu}\n {fiyat}\n";
+            return s;
+        }
+        public decimal IndirimYap(int oran)
+        {
+            decimal indirimliFiyat = fiyat - (fiyat * oran / 100);
+            return indirimliFiyat;
+        }
+        public decimal FiyatArttir(int oran) 
+        { 
+            decimal zamliFiyat = fiyat +(fiyat * oran / 100);
+            return zamliFiyat;
+        }
+    }
+}
---
+++ 2025012/ClassOrnek1/ClassOrnek1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassOrnek1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Araba yeniAraba;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yeniAraba = new Araba("Honda", "Civic", "LPG", 1500000);
+            label1.Text = yeniAraba.BilgilerYazdir();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "satış fiyatı :"+yeniAraba.IndirimYap(15);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "satış fiyatı :" + yeniAraba.FiyatArttir(25);
+        }
+    }
+}
